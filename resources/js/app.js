396de0153583340c20a4852de8b7cedd0edefa79
quyetnguyen02 <quyetnguyen0804@gmail.com>
2026-07-31 06:26:29 +0000

    hot file add to card js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import './bootstrap';
 import './UserPage/banner.js';
-import './UserPage/home.js';
+import './UserPage/modalgetNumber.js';
 // import './UserPage/card.js';
 // import './UserPage/modal.js';
 // import './UserPage/thumb.js';
