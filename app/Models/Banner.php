65097ff6bdc293881714c2289296588c4040fb10
quyetnguyen02 <quyetnguyen0804@gmail.com>
2026-08-03 03:58:54 +0000

    update banner

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Banner extends Model
 {
-    protected $table = 'banners';
+    protected $table = 'banners_p';
 
 
     function getBannersDisplay(): \Illuminate\Database\Eloquent\Collection
